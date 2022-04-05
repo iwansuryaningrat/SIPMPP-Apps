5ae5dab1ff3121dd4fea86643fdcf734e90a215b
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-04-05 22:35:03 +0700

    add multiple select dropdown at unit field

--- app/Views/admin/add-auditor.php
+++ app/Views/admin/add-auditor.php
@@ -27,36 +27,47 @@
   <form method="POST" action="/admin/addbasicuser/auditor">
     <!-- User -->
     <div class="row mb-3 mb-sm-4">
-      <label for="user" class="col-lg-3 col-md-3 col-sm-4 col-form-label form__label">User <span class="color__danger">*</span></label>
+      <label for="user" class="col-lg-3 col-md-3 col-sm-4 col-form-label form__label">User <span
+          class="color__danger">*</span></label>
       <div class="col-lg-6 col-md-9 col-sm-8">
         <select name="user" id="user" class="form-select form__select shadow-none" required>
           <option disabled selected>Pilih User</option>
           <?php foreach ($users as $user) : ?>
-            <option value="<?= $user['email'] ?>"><?= $user['nama']; ?></option>
+          <option value="<?= $user['email'] ?>">
+            <?= $user['nama']; ?>
+          </option>
           <?php endforeach; ?>
         </select>
       </div>
     </div>
     <!-- unit -->
     <div class="row mb-3 mb-sm-4">
-      <label for="unit" class="col-lg-3 col-md-3 col-sm-4 col-form-label form__label">Unit <span class="color__danger">*</span></label>
+      <label for="unit" class="col-lg-3 col-md-3 col-sm-4 col-form-label form__label">Unit <span
+          class="color__danger">*</span></label>
       <div class="col-lg-6 col-md-9 col-sm-8">
-        <select name="unit" id="unit" class="form-select form__select shadow-none" required>
-          <option disabled selected>Pilih Unit</option>
+        <select name="unit" id="unit" class="form-select form__select shadow-none" multiple multiselect-search="true"
+          multiselect-select-all="true" multiselect-max-items="3" onchange="console.log(this.selectedOptions)" required>
           <?php foreach ($units as $unit) : ?>
-            <option value="<?= $unit['unit_id'] ?>"><?= $unit['nama_unit']; ?></option>
+          <option
+            value="<?= $unit['unit_id'] ?>">
+            <?= $unit['nama_unit']; ?>
+          </option>
           <?php endforeach; ?>
         </select>
       </div>
     </div>
     <!-- tahun -->
     <div class="row mb-3 mb-sm-4">
-      <label for="tahun" class="col-lg-3 col-md-3 col-sm-4 col-form-label form__label">Tahun <span class="color__danger">*</span></label>
+      <label for="tahun" class="col-lg-3 col-md-3 col-sm-4 col-form-label form__label">Tahun <span
+          class="color__danger">*</span></label>
       <div class="col-lg-6 col-md-9 col-sm-8">
         <select name="tahun" id="tahun" class="form-select form__select shadow-none" required>
           <option disabled selected>Pilih Tahun</option>
           <?php foreach ($tahuns as $tahuns) : ?>
-            <option value="<?= $tahuns['tahun'] ?>"><?= $tahuns['tahun'] ?></option>
+          <option
+            value="<?= $tahuns['tahun'] ?>">
+            <?= $tahuns['tahun'] ?>
+          </option>
           <?php endforeach; ?>
 
         </select>
@@ -77,9 +88,8 @@
 <?= $this->endSection(); ?>
 
 <?= $this->section('script'); ?>
-
+<script src="/admin/assets/js/multipleselect-dropdown.js"></script>
 <script>
-
 </script>
 
-<?= $this->endSection(); ?>
+<?= $this->endSection();
