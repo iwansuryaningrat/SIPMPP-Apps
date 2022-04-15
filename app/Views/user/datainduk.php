51d6d6c767ca1fc6c7323189f960a40c3d71b586
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-04-15 09:05:06 +0700

    Update Validation On Each Form of The Role User
    add class shadow-btn in button class and update jquery validation

--- app/Views/user/datainduk.php
+++ app/Views/user/datainduk.php
@@ -23,10 +23,14 @@
 <!-- filter -->
 <div class="filter__table">
   <div class="nav nav-pills" id="pills-tab" role="tablist">
-    <button class="btn filter__btn me-0 me-md-3 shadow-none active nav-link active" id="pills-datainduk-penelitian" data-bs-toggle="pill" data-bs-target="#pills-table-datainduk-penelitian" type="button" role="tab" aria-controls="pills-table-datainduk-penelitian" aria-selected="true">
+    <button class="btn filter__btn me-0 me-md-3 shadow-none active nav-link active" id="pills-datainduk-penelitian"
+      data-bs-toggle="pill" data-bs-target="#pills-table-datainduk-penelitian" type="button" role="tab"
+      aria-controls="pills-table-datainduk-penelitian" aria-selected="true">
       Penelitian
     </button>
-    <button class="btn filter__btn shadow-none nav-link" id="pills-datainduk-pm" data-bs-toggle="pill" data-bs-target="#pills-table-datainduk-pm" type="button" role="tab" aria-controls="pills-table-datainduk-pm" aria-selected="false">
+    <button class="btn filter__btn shadow-none nav-link" id="pills-datainduk-pm" data-bs-toggle="pill"
+      data-bs-target="#pills-table-datainduk-pm" type="button" role="tab" aria-controls="pills-table-datainduk-pm"
+      aria-selected="false">
       Pengabdian Masyarakat
     </button>
   </div>
@@ -38,7 +42,8 @@
   <?= session()->getFlashdata('message'); ?>
 
   <!-- penelitian -->
-  <div class="tab-pane fade show active" id="pills-table-datainduk-penelitian" role="tabpanel" aria-labelledby="pills-datainduk-penelitian">
+  <div class="tab-pane fade show active" id="pills-table-datainduk-penelitian" role="tabpanel"
+    aria-labelledby="pills-datainduk-penelitian">
     <div class="sipmpp__table">
       <div class="table-responsive">
         <table class="table table__datainduk__content sipmpp__table-content table-hover" id="datainduk-penelitian">
@@ -53,19 +58,27 @@
           </thead>
           <tbody>
             <?php foreach ($data_indukPen as $datainduk) : ?>
-              <tr>
-                <td><?= $i; ?>
-                </td>
-                <td><?= $datainduk['kode']; ?>
-                </td>
-                <td><?= $datainduk['nama_induk']; ?>
-                </td>
-                <td><?= $datainduk['nilai']; ?>
-                </td>
-                <td>
-                  <a role="button" data-bs-toggle="modal" data-bs-placement="top" title="Edit" href="#staticBackdrop" class="edit__data__induk__icon" data-id="<?= $datainduk['induk_id']; ?>" data-kode="<?= $datainduk['kode']; ?>" data-kategori="<?= $datainduk['nama_kategori']; ?>" data-katid="<?= $datainduk['kategori_id']; ?>" data-kebutuhan-data="<?= $datainduk['nama_induk']; ?>" data-nilai="<?= $datainduk['nilai']; ?>"><i class="fa-solid fa-pen-to-square"></i></a>
-                </td>
-              </tr>
+            <tr>
+              <td><?= $i; ?>
+              </td>
+              <td><?= $datainduk['kode']; ?>
+              </td>
+              <td><?= $datainduk['nama_induk']; ?>
+              </td>
+              <td><?= $datainduk['nilai']; ?>
+              </td>
+              <td>
+                <a role="button" data-bs-toggle="modal" data-bs-placement="top" title="Edit" href="#staticBackdrop"
+                  class="edit__data__induk__icon"
+                  data-id="<?= $datainduk['induk_id']; ?>"
+                  data-kode="<?= $datainduk['kode']; ?>"
+                  data-kategori="<?= $datainduk['nama_kategori']; ?>"
+                  data-katid="<?= $datainduk['kategori_id']; ?>"
+                  data-kebutuhan-data="<?= $datainduk['nama_induk']; ?>"
+                  data-nilai="<?= $datainduk['nilai']; ?>"><i
+                    class="fa-solid fa-pen-to-square"></i></a>
+              </td>
+            </tr>
             <?php $i++;
             endforeach; ?>
           </tbody>
@@ -91,19 +104,27 @@
           <tbody>
             <?php $i = 1;
             foreach ($data_indukPPM as $datainduk) : ?>
-              <tr>
-                <td><?= $i; ?>
-                </td>
-                <td><?= $datainduk['kode']; ?>
-                </td>
-                <td><?= $datainduk['nama_induk']; ?>
-                </td>
-                <td><?= $datainduk['nilai']; ?>
-                </td>
-                <td>
-                  <a role="button" data-bs-toggle="modal" data-bs-placement="top" title="Edit" href="#staticBackdrop" class="edit__data__induk__icon" data-id="<?= $datainduk['induk_id']; ?>" data-kode="<?= $datainduk['kode']; ?>" data-kategori="<?= $datainduk['nama_kategori']; ?>" data-katid="<?= $datainduk['kategori_id']; ?>" data-kebutuhan-data="<?= $datainduk['nama_induk']; ?>" data-nilai="<?= $datainduk['nilai']; ?>"><i class="fa-solid fa-pen-to-square"></i></a>
-                </td>
-              </tr>
+            <tr>
+              <td><?= $i; ?>
+              </td>
+              <td><?= $datainduk['kode']; ?>
+              </td>
+              <td><?= $datainduk['nama_induk']; ?>
+              </td>
+              <td><?= $datainduk['nilai']; ?>
+              </td>
+              <td>
+                <a role="button" data-bs-toggle="modal" data-bs-placement="top" title="Edit" href="#staticBackdrop"
+                  class="edit__data__induk__icon"
+                  data-id="<?= $datainduk['induk_id']; ?>"
+                  data-kode="<?= $datainduk['kode']; ?>"
+                  data-kategori="<?= $datainduk['nama_kategori']; ?>"
+                  data-katid="<?= $datainduk['kategori_id']; ?>"
+                  data-kebutuhan-data="<?= $datainduk['nama_induk']; ?>"
+                  data-nilai="<?= $datainduk['nilai']; ?>"><i
+                    class="fa-solid fa-pen-to-square"></i></a>
+              </td>
+            </tr>
             <?php $i++;
             endforeach; ?>
           </tbody>
@@ -118,7 +139,8 @@
 <?= $this->section('modal'); ?>
 
 <!-- Modal -->
-<div class="modal fade edit__datainduk__modal" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="modal-data-induk" aria-hidden="true">
+<div class="modal fade edit__datainduk__modal" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false"
+  tabindex="-1" aria-labelledby="modal-data-induk" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content modal__content">
       <div class="modal-header modal__header">
@@ -128,14 +150,15 @@
         <h4 class="modal-title" id="modal-data-induk">Nilai Data Induk</h4>
 
         <!-- form -->
-        <form class="modal__form" method="POST" action="/home/editdatainduk">
+        <form class="modal__form" method="POST" action="/home/editdatainduk" id="formEditInduk">
           <!-- id input -->
           <input type="hidden" id="id" name="induk_id" />
           <input type="hidden" id="kategori_id" name="kategori_id" />
           <!-- kategori -->
           <div class="modal__form-content">
             <label for="kategori" class="form-label form__label">Kategori</label>
-            <input type="text" class="form-control shadow-none form__control" id="nama_kategori" name="nama_kategori" disabled required />
+            <input type="text" class="form-control shadow-none form__control" id="nama_kategori" name="nama_kategori"
+              disabled required />
           </div>
           <!-- kode -->
           <div class="modal__form-content">
@@ -145,20 +168,22 @@
           <!-- kebutuhan data -->
           <div class="modal__form-content">
             <label for="kebutuhan-data" class="form-label form__label">Kebutuhan Data</label>
-            <textarea id="kebutuhan-data" class="form-control shadow-none form__control" cols="30" rows="3" name="kebutuhan" disabled required>
+            <textarea id="kebutuhan-data" class="form-control shadow-none form__control" cols="30" rows="3"
+              name="kebutuhan" disabled required>
                 </textarea>
           </div>
           <!-- nilai -->
           <div class="modal__form-content">
             <label for="nilai" class="form-label form__label">Nilai</label>
-            <input type="text" class="form-control shadow-none form__control" id="nilai" name="nilai" required autocomplete="off" onkeypress="javascript: return validationNumber(event)" />
+            <input type="text" class="form-control shadow-none form__control" id="nilai" name="nilai" required
+              autocomplete="off" onkeypress="javascript: return validationNumber(event)" />
           </div>
           <div id="alert-wrong-text"></div>
           <div class="modal__form-btn">
             <button type="button" class="btn cancel__btn shadow-none" data-bs-dismiss="modal">
               Batal
             </button>
-            <button type="submit" class="btn modal__btn shadow-none">Kirim</button>
+            <button type="submit" class="btn modal__btn shadow-none" id="btnEditInduk">Kirim</button>
           </div>
         </form>
       </div>
@@ -169,7 +194,10 @@
 <?= $this->endSection(); ?>
 
 <?= $this->section('userscript'); ?>
-
+<!-- jquery validate -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.min.js"
+  integrity="sha512-37T7leoNS06R80c8Ulq7cdCDU5MNQBwlYoy1TX/WUsLFC2eYNqtKlV0QjH7r8JpG/S0GUMZwebnVFLPd6SU5yg=="
+  crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script>
   // active filer button
   $(function() {
@@ -225,6 +253,28 @@
 
     return true;
   };
+
+  // validate form jquery
+  const exclamationCircle = "<i class='fa-solid fa-circle-exclamation'></i>";
+
+  $(document).ready(function() {
+    $("#formEditInduk").validate({
+      rules: {
+        nilai: {
+          required: true,
+        },
+      },
+      messages: {
+        nilai: {
+          required: exclamationCircle + " Nilai is required.",
+        },
+      },
+    });
+
+    $("#btnEditInduk").on("click", () => {
+      console.log($("#formEditInduk").valid());
+    });
+  });
 </script>
 
 <?= $this->endSection();
