4e1fcae09df380534972844dbf97614aec9a6f10
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-04-19 23:33:15 +0700

    Checking Form Reset

--- app/Views/admin/reset-penilaian.php
+++ app/Views/admin/reset-penilaian.php
@@ -27,34 +27,22 @@
     <form method="POST" action="#">
         <!-- tahun -->
         <div class="row mb-3 mb-sm-4">
-            <label for="tahun" class="col-lg-3 col-md-3 col-sm-4 col-form-label form__label">Tahun <span
-                    class="color__danger">*</span></label>
+            <label for="tahun" class="col-lg-3 col-md-3 col-sm-4 col-form-label form__label">Tahun <span class="color__danger">*</span></label>
             <div class="col-lg-6 col-md-9 col-sm-8">
-                <select name="tahun[]" id="tahun" class="form-select form__select shadow-none" multiple
-                    multiselect-search="true" multiselect-select-all="true" multiselect-max-items="200"
-                    onchange="console.log(this.selectedOptions)" required placeholder-inputs="Pilih Tahun">
+                <select name="tahun[]" id="tahun" class="form-select form__select shadow-none" multiple multiselect-search="true" multiselect-select-all="true" multiselect-max-items="200" onchange="console.log(this.selectedOptions)" required placeholder-inputs="Pilih Tahun">
                     <?php foreach ($daftartahun as $datatahun) : ?>
-                    <option
-                        value="<?= $datatahun['tahun'] ?>">
-                        <?= $datatahun['tahun'] ?>
-                    </option>
+                        <option value="<?= $datatahun['tahun'] ?>"><?= $datatahun['tahun'] ?></option>
                     <?php endforeach; ?>
                 </select>
             </div>
         </div>
         <!-- unit -->
         <div class="row mb-3 mb-sm-4">
-            <label for="unit" class="col-lg-3 col-md-3 col-sm-4 col-form-label form__label">Unit <span
-                    class="color__danger">*</span></label>
+            <label for="unit" class="col-lg-3 col-md-3 col-sm-4 col-form-label form__label">Unit <span class="color__danger">*</span></label>
             <div class="col-lg-6 col-md-9 col-sm-8">
-                <select name="unit[]" id="unit" class="form-select form__select shadow-none" multiple
-                    multiselect-search="true" multiselect-select-all="true" multiselect-max-items="200"
-                    onchange="console.log(this.selectedOptions)" required placeholder-inputs="Pilih Unit">
+                <select name="unit[]" id="unit" class="form-select form__select shadow-none" multiple multiselect-search="true" multiselect-select-all="true" multiselect-max-items="200" onchange="console.log(this.selectedOptions)" required placeholder-inputs="Pilih Unit">
                     <?php foreach ($daftarunit as $dataunit) : ?>
-                    <option
-                        value="<?= $dataunit['unit_id'] ?>">
-                        <?= $dataunit['nama_unit'] ?>
-                    </option>
+                        <option value="<?= $dataunit['unit_id'] ?>"><?= $dataunit['nama_unit'] ?></option>
                     <?php endforeach; ?>
                 </select>
             </div>
@@ -63,9 +51,7 @@
         <div class="row">
             <div class="col-lg-9 col-md-12 col-sm-12 button__section">
                 <a href="/admin/penilaian" class="btn form__btn cancel__btn me-4 shadow-none" role="button">Batal</a>
-                <button type="submit" class="btn form__btn btn-danger shadow-none">
-                    Reset
-                </button>
+                <button type="submit" class="btn form__btn btn-danger shadow-none">Reset</button>
             </div>
         </div>
     </form>
