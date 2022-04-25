8e98d43d5276368a17f6ab7fd84c00189012a747
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-04-25 12:00:59 +0700

    Remove useless javascript in file
    remove function validationNumber

--- app/Views/auditor/datainduk.php
+++ app/Views/auditor/datainduk.php
@@ -121,15 +121,6 @@
       return false;
     });
   });
-
-  // validation number
-  let validationNumber = (evt) => {
-    var iKeyCode = evt.which ? evt.which : evt.keyCode;
-    if (iKeyCode != 46 && iKeyCode > 31 && (iKeyCode < 48 || iKeyCode > 57))
-      return false;
-
-    return true;
-  };
 </script>
 
 <?= $this->endSection();
