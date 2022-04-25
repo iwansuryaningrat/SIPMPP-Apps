49474686eab384d68b0ff5a7fda5232fa3bdbace
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-04-25 14:45:04 +0700

    Profile Leader 100%
    Update responsiveness, content, environment, and css

--- app/Views/leader/profile.php
+++ app/Views/leader/profile.php
@@ -32,19 +32,23 @@
         <h5>Profile Pengguna</h5>
         <hr class="head__hr" />
         <?= session()->getFlashdata('profilemessage'); ?>
-        <form class="form__profile" id="formChangeInfo" action="/editdata/editprofile" enctype="multipart/form-data" method="POST">
+        <form class="form__profile" id="formChangeInfo" action="/editdata/editprofile" enctype="multipart/form-data"
+            method="POST">
             <!-- foto -->
             <div>
                 <label for="photo-profile" class="form-label form__label">Foto Profil</label>
                 <div class="input-group input-group__photo">
                     <div class="img__input-photo mb-3">
                         <div class="img__photo-field">
-                            <img src="/profile/<?= $data_user['foto']; ?>" alt="photo-profile" class="img__input" id="img-input-preview" />
+                            <img src="/profile/<?= $data_user['foto']; ?>"
+                                alt="photo-profile" class="img__input" id="img-input-preview" />
                         </div>
                     </div>
                     <div class="img__input-field">
-                        <input type="file" class="form-control form__control__photo" id="photo-profile" aria-labelledby="photo-notice" onchange="previewImage(this)" name="photo-profile" />
-                        <label class="form__label__photo btn btn__dark" for="photo-profile">Ubah Profile</label>
+                        <input type="file" class="form-control form__control__photo" id="photo-profile"
+                            aria-labelledby="photo-notice" onchange="previewImage(this)" name="photo-profile" />
+                        <label class="form__label__photo btn btn__dark ellipsis__text" for="photo-profile">Ubah
+                            Profile</label>
                         <label id="photo-notice" class="form-text form__text mb-3">
                             Gambar profil Anda sebaiknya memiliki raiso 1:1 dan berukuran tidak lebih dari 2 MB.
                         </label>
@@ -55,26 +59,34 @@
             <!-- Nama lengkap -->
             <div class="mb-3">
                 <label for="fullname" class="form-label form__label">Nama Lengkap</label>
-                <input type="text" class="form-control form__control shadow-none" id="fullname" value="<?= $data_user['nama']; ?>" name="fullname" required />
+                <input type="text" class="form-control form__control shadow-none" id="fullname"
+                    value="<?= $data_user['nama']; ?>"
+                    name="fullname" required />
             </div>
             <!-- email -->
             <div class="mb-3">
                 <label for="email" class="form-label form__label">Email</label>
-                <input type="text" class="form-control form__control shadow-none" id="email" name="email" disabled value="<?= $data_user['email']; ?>" required />
+                <input type="text" class="form-control form__control shadow-none" id="email" name="email" disabled
+                    value="<?= $data_user['email']; ?>"
+                    required />
             </div>
             <!-- nip -->
             <div class="mb-3">
                 <label for="nip" class="form-label form__label">NIP</label>
-                <input type="text" class="form-control form__control shadow-none" id="nip" name="nip" value="<?= $user['nip']; ?>" required />
+                <input type="text" class="form-control form__control shadow-none" id="nip" name="nip"
+                    value="<?= $user['nip']; ?>"
+                    required />
             </div>
             <!-- nomor telepon -->
             <div class="mb-3 mb__big">
                 <label for="no-telp" class="form-label form__label">Nomor telepon</label>
-                <input type="text" class="form-control form__control shadow-none" id="no-telp" name="no-telp" value="<?= $user['telp']; ?>" required />
+                <input type="text" class="form-control form__control shadow-none" id="no-telp" name="no-telp"
+                    value="<?= $user['telp']; ?>"
+                    required />
             </div>
             <!-- button -->
             <div class="d-flex justify-content-end">
-                <button type="submit" class="btn btn__dark shadow-none">
+                <button type="submit" class="btn btn__dark shadow-none ellipsis__text">
                     Simpan Perubahan
                 </button>
             </div>
@@ -88,27 +100,36 @@
             <hr />
             <?= session()->getFlashdata('pwdmessage'); ?>
             <form class="form__change__password" id="formChangePassword" action="/editdata/editpassword" method="POST">
-                <div class="mb-3 position-relative">
-                    <label for="oldPassword" class="form-label form__label">Password Lama <span class="color__danger">*</span></label>
-                    <input type="password" class="form-control form__control shadow-none" id="oldPassword" name="oldPassword" autocomplete="off" required />
-                    <span id="toggleOldPassword"><i class="fa-solid fa-eye icon__hide__password" title="show password"></i></span>
+                <div class="mb-3 position-relative old__pass">
+                    <label for="oldPassword" class="form-label form__label">Password Lama <span
+                            class="color__danger">*</span></label>
+                    <input type="password" class="form-control form__control shadow-none" id="oldPassword"
+                        name="oldPassword" autocomplete="off" required />
+                    <span id="toggleOldPassword"><i class="fa-solid fa-eye icon__hide__password"
+                            title="show password"></i></span>
                 </div>
-                <div class="mb-3 position-relative">
-                    <label for="newPassword" class="form-label form__label">Password Baru <span class="color__danger">*</span></label>
-                    <input type="password" class="form-control form__control shadow-none" id="newPassword" name="newPassword" aria-labelledby="new-password-notice" autocomplete="off" required />
-                    <span id="toggleNewPassword"><i class="fa-solid fa-eye icon__hide__password" title="show password"></i></span>
+                <div class="mb-3 position-relative new__pass">
+                    <label for="newPassword" class="form-label form__label">Password Baru <span
+                            class="color__danger">*</span></label>
+                    <input type="password" class="form-control form__control shadow-none" id="newPassword"
+                        name="newPassword" aria-labelledby="new-password-notice" autocomplete="off" required />
+                    <span id="toggleNewPassword"><i class="fa-solid fa-eye icon__hide__password"
+                            title="show password"></i></span>
                     <div id="new-password-notice" class="form-text form__text">
                         Gunakan minimal 8 karakter dengan kombinasi huruf dan angka.
                     </div>
                 </div>
-                <div class="mb-3 mb__big position-relative">
+                <div class="mb-3 mb__big position-relative confirm__pass">
                     <label for="newPasswordConfirm" class="form-label form__label">Konfirmasi Password Baru
                         <span class="color__danger">*</span></label>
-                    <input type="password" class="form-control form__control shadow-none" name="newPasswordConfirm" id="newPasswordConfirm" autocomplete="off" required />
-                    <span id="toggleNewPasswordConfirm"><i class="fa-solid fa-eye icon__hide__password" title="show password"></i></span>
+                    <input type="password" class="form-control form__control shadow-none" name="newPasswordConfirm"
+                        id="newPasswordConfirm" autocomplete="off" required />
+                    <span id="toggleNewPasswordConfirm"><i class="fa-solid fa-eye icon__hide__password"
+                            title="show password"></i></span>
                 </div>
                 <div class="d-flex justify-content-end">
-                    <button type="submit" class="btn btn__light shadow-none" id="btnSubmitChangePassword">
+                    <button type="submit" class="btn btn__light shadow-none ellipsis__text"
+                        id="btnSubmitChangePassword">
                         Ubah Password
                     </button>
                 </div>
@@ -121,7 +142,9 @@
 
 <?= $this->section('script'); ?>
 <!-- jquery validate -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.min.js" integrity="sha512-37T7leoNS06R80c8Ulq7cdCDU5MNQBwlYoy1TX/WUsLFC2eYNqtKlV0QjH7r8JpG/S0GUMZwebnVFLPd6SU5yg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.min.js"
+    integrity="sha512-37T7leoNS06R80c8Ulq7cdCDU5MNQBwlYoy1TX/WUsLFC2eYNqtKlV0QjH7r8JpG/S0GUMZwebnVFLPd6SU5yg=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script>
     // preview image and validation
     previewImage = (input) => {
@@ -179,12 +202,12 @@
                 },
                 newPassword: {
                     required: exclamationCircle + " Password Baru is required.",
-                    minlength: "Password baru minimal 8 karakter",
+                    minlength: exclamationCircle + " Your Password must be at least 8 characters.",
                 },
                 newPasswordConfirm: {
                     required: exclamationCircle + " Please enter the password again.",
                     equalTo: exclamationCircle +
-                        " Your Konfirmasi Password does not match the Password Baru",
+                        " Your Konfirmasi Password does not match the Password Baru.",
                 },
             },
         });
@@ -217,6 +240,8 @@
                 },
                 'no-telp': {
                     required: exclamationCircle + " Nomor Telepon is required.",
+                    minlength: exclamationCircle +
+                        " Your Nomor Telepon must be at least 12 characters.",
                 },
             }
         });
