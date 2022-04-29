7cede740936d6380a9662b772aa3c6059a05e2c9
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-04-30 01:07:36 +0700

    AUDITOR 100%
    Update css environment

--- public/auditor/js/scripts.js
+++ public/auditor/js/scripts.js
@@ -1,19 +1,7 @@
 // change button carousel index video
 // sidebar open-close
 $(document).click((e) => {
-  if (
-    // e.target.id !== "header-main-nav-btn" &&
-    // e.target.id !== "sidebar-content" &&
-    // e.target.id !== "header-main-nav-btn-i" &&
-    // e.target.id !== "header-main-nav-dropdown" &&
-    // e.target.id !== "btn-dropdown" &&
-    // e.target.id !== "photo-dropdown" &&
-    // e.target.id !== "line1" &&
-    // e.target.id !== "line2" &&
-    // e.target.id !== "line3"
-    e.target.id === "sidebar-main-icon" ||
-    e.target.nodeName === "path"
-  ) {
+  if (e.target.id === "sidebar-main-icon" || e.target.nodeName === "path") {
     $("#sidebar-content").removeClass("minimize__sidebar");
     $("#main-content").removeClass("minimize__sidebar");
     $(".header__mini").removeClass("active");
@@ -27,4 +15,5 @@ $("#header-main-nav-btn-i").click(() => {
   $(".header__mini").toggleClass("active");
   $(".header__big").toggleClass("active");
   $(".collapse.collapse__dropside").removeClass("show");
+  $("html").toggleClass("nav__open");
 });
