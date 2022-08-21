2b36ff0231ce5d8f19c998718058bbeebe5affb9
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-08-21 16:44:26 +0700

    update responsives login page

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -10,8 +10,7 @@
 
     <!-- meta data -->
     <meta name="title" content="SIPMPP UNDIP">
-    <meta name="description"
-        content="SIPMPP merupakan Sistem Informasi Penjaminan Mutu Penelitian dan Pengabdian Universitas Diponegoro.">
+    <meta name="description" content="SIPMPP merupakan Sistem Informasi Penjaminan Mutu Penelitian dan Pengabdian Universitas Diponegoro.">
     <meta name="keywords" content="sipmpp, sipma, undip, penelitian, pengabdian, mutu">
     <meta name="robots" content="index, follow">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
@@ -20,14 +19,11 @@
     <meta name="copyright" content="© 2022 teamsipmpppundip">
 
     <!-- bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" />
     <!-- fontawesome -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.0/css/solid.css"
-        integrity="sha384-ltWlpN+Dl8XfKEnC9oW+dDRF8Z7jsYkxQ/WMRoJ2VHH5G2nQZ4if2NWwmV0ybzZ7" crossorigin="anonymous" />
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.0/css/fontawesome.css"
-        integrity="sha384-RLM8Rxp/DcBfCfSI3bGwwoMMxxy34D2e58WAqXmmdnh0WYlAQ8jeOB3A1ed5KUSm" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.0/css/solid.css" integrity="sha384-ltWlpN+Dl8XfKEnC9oW+dDRF8Z7jsYkxQ/WMRoJ2VHH5G2nQZ4if2NWwmV0ybzZ7" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.0/css/fontawesome.css" integrity="sha384-RLM8Rxp/DcBfCfSI3bGwwoMMxxy34D2e58WAqXmmdnh0WYlAQ8jeOB3A1ed5KUSm" crossorigin="anonymous" />
     <!-- custom -->
     <link rel="stylesheet" href="/assets/css/styles-login.css" />
 
@@ -50,15 +46,11 @@
             <!-- left -->
             <div class="login__content-left">
                 <div class="content-left__bg">
-                    <div id="carouselExampleCaptions" class="carousel slide content-left__carousel"
-                        data-bs-ride="carousel">
+                    <div id="carouselExampleCaptions" class="carousel slide content-left__carousel" data-bs-ride="carousel">
                         <div class="carousel-indicators">
-                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0"
-                                class="active" aria-current="true" aria-label="Slide 1"></button>
-                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
-                                aria-label="Slide 2"></button>
-                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
-                                aria-label="Slide 3"></button>
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
                         </div>
                         <div class="carousel-inner carousel__inner">
                             <div class="carousel-item active carousel__item">
@@ -83,10 +75,7 @@
                                 <h3 class="carousel__item-title">Gedung ICT Center</h3>
                                 <div class="carousel__item-map__content">
                                     <div class="carousel__item-map">
-                                        <iframe
-                                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3959.6120051633497!2d110.43685551478825!3d-7.054793794904621!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e708c1d399ff9f9%3A0x2d2033fed8e955dd!2sGedung%20ICT%20Undip!5e0!3m2!1sen!2sid!4v1648699051976!5m2!1sen!2sid"
-                                            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
-                                            referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3959.6120051633497!2d110.43685551478825!3d-7.054793794904621!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e708c1d399ff9f9%3A0x2d2033fed8e955dd!2sGedung%20ICT%20Undip!5e0!3m2!1sen!2sid!4v1648699051976!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                                     </div>
                                 </div>
                                 <div class="carousel-caption carousel__caption d-none d-md-block">
@@ -111,53 +100,48 @@
                 <div class="login__content-right__body">
                     <h2 class="login__title">Login</h2>
                     <?php if (!session()->getFlashdata('gagal')) : ?>
-                    <p class="login__desc mb-4">
-                        Selamat datang kembali! Silahkan masukkan detail Anda dibawah ini.
-                    </p>
+                        <p class="login__desc mb-4">
+                            Selamat datang kembali! Silahkan masukkan detail Anda dibawah ini.
+                        </p>
                     <?php endif; ?>
 
                     <!-- Mengecek apakah ada flash data -->
                     <?php if (session()->getFlashdata('gagal')) : ?>
-                    <!-- alert danger -->
-                    <div class="alert alert-danger d-flex alert-dismissible" role="alert" style="padding-right: 2.5rem">
-                        <i class="bi bi-exclamation-triangle-fill d-block pe-3" style="font-size: 1.25rem"></i>
-                        <div>
-                            <!-- Menampilkan flashdata gagal -->
-                            <?= session()->getFlashdata('gagal'); ?>
+                        <!-- alert danger -->
+                        <div class="alert alert-danger d-flex alert-dismissible" role="alert" style="padding-right: 2.5rem">
+                            <i class="bi bi-exclamation-triangle-fill d-block pe-3" style="font-size: 1.25rem"></i>
+                            <div>
+                                <!-- Menampilkan flashdata gagal -->
+                                <?= session()->getFlashdata('gagal'); ?>
+                            </div>
                         </div>
-                    </div>
-                    <!-- end alert danger -->
+                        <!-- end alert danger -->
                     <?php endif; ?>
 
                     <?php if (session()->getFlashdata('success')) : ?>
-                    <!-- alert success -->
-                    <div class="alert alert-success d-flex alert-dismissible" role="alert"
-                        style="padding-right: 2.5rem">
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"
-                            style="padding: 1.25rem"></button>
-                        <i class="bi bi-check-circle-fill d-block pe-3" style="font-size: 1.25rem"></i>
-                        <div>
-                            <!-- Menampilkan flashdata success -->
-                            <?= session()->getFlashdata('success'); ?>
+                        <!-- alert success -->
+                        <div class="alert alert-success d-flex alert-dismissible" role="alert" style="padding-right: 2.5rem">
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" style="padding: 1.25rem"></button>
+                            <i class="bi bi-check-circle-fill d-block pe-3" style="font-size: 1.25rem"></i>
+                            <div>
+                                <!-- Menampilkan flashdata success -->
+                                <?= session()->getFlashdata('success'); ?>
+                            </div>
                         </div>
-                    </div>
-                    <!-- end alert success -->
+                        <!-- end alert success -->
                     <?php endif; ?>
                     <!-- form login -->
                     <form class="form__login" method="POST" action="/auth/formloginunit">
                         <div class="mb-4 position-relative">
                             <label for="email" class="form-label form__label">Email</label>
-                            <input type="email" class="form-control form__control shadow-none" name="email" id="email"
-                                placeholder="Masukkan email" required autocomplete="off" />
+                            <input type="email" class="form-control form__control shadow-none" name="email" id="email" placeholder="Masukkan email" required autocomplete="off" />
                             <i class="fa-solid fa-at icon__form"></i>
                         </div>
                         <div class="mb-5 position-relative">
                             <label for="password" class="form-label form__label">Password</label>
-                            <input type="password" class="form-control form__control shadow-none" name="password"
-                                id="password" placeholder="Masukkan password" required />
+                            <input type="password" class="form-control form__control shadow-none" name="password" id="password" placeholder="Masukkan password" required />
                             <i class="fa-solid fa-lock icon__form"></i>
-                            <span id="togglePassword"><i class="fa-solid fa-eye icon__hide__password"
-                                    title="show password"></i></span>
+                            <span id="togglePassword"><i class="fa-solid fa-eye icon__hide__password" title="show password"></i></span>
                         </div>
                         <button type="submit" class="btn login__btn shadow-none">Login</button>
                     </form>
@@ -168,19 +152,14 @@
 
     <!-- scripts -->
     <!-- jquery -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
-        integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <!-- bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
-        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
-        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
     </script>
     <!-- fontawesome -->
-    <script defer src="https://use.fontawesome.com/releases/v6.1.0/js/all.js"
-        integrity="sha384-vLLEq/Un/eZFmXAu4Xxf8F00RSSMzPcI7iDiT6hpB4zFpezCEGhb5daeR8PLyrLI" crossorigin="anonymous">
+    <script defer src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" integrity="sha384-vLLEq/Un/eZFmXAu4Xxf8F00RSSMzPcI7iDiT6hpB4zFpezCEGhb5daeR8PLyrLI" crossorigin="anonymous">
     </script>
     <!-- custom -->
     <script>
